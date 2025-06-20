f26aa7e11c7bd1f084a2e2f69a22b58f6dd412b8
putrita <putritandriyani@gmail.com>
2025-06-20 23:54:10 +0800

    index.js diupdate

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
 app.listen(port, () => {
   console.log(`Gemini chatbot running on http://localhost:${port}`);
 });
+
+app.post('/api/chat', async (req, res) => {
+    const userMessage = req.body.message;
+    if (!userMessage){
+        return res.status(400).json({ reply: "Message is required." });
+    }
+
+    try{
+        const result = await model.generateContent(userMessage);
+        const response = await result.response;
+        const text = response.text();
+
+        res.json({reply: text });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error: "Something went wrong"});
+    
+    }
+});
